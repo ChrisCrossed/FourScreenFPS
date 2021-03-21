37f5edc4d375ecca183cb9b19a7218ae87429a78
ChrisCrossed <chriscrossed.86@gmail.com>
2021-03-21 14:39:22 -0700

    Player Controller Updates
    Implementing basic player controller settings using XInput

--- 4ScreenFPS/Assets/PlayerController.cs
+++ 4ScreenFPS/Assets/PlayerController.cs
@@ -2,16 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class temp : MonoBehaviour
+
+public class PlayerController : PLAYER_INPUT
 {
+    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        InputStart();
     }
 
     // Update is called once per frame
     void Update()
     {
+        InputUpdate();
+
+        if( playerState.Buttons.A == XInputDotNetPure.ButtonState.Pressed )
+        {
+            print("A Held");
+        }
     }
 }
